9c663a67fe32a63078e86162882e1b3c05410435
artsyompichinko <artsyompichinko@gmail.com>
2024-03-13 20:12:44 +0300

    Add telebot and test answer to message

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('Hello world')
+import telebot
+import time
+from  bot_token import token as tk
+
+bot = telebot.TeleBot(tk) # Вместо tk используй свой токен тг бота
+
+@bot.message_handler(commands=['start'])
+def first_use(message):
+    bot.send_message(message.from_user.id, 'test_message')
+
+if __name__ == '__main__':
+    while True:
+        try:
+            bot.polling(none_stop=True)
+        except Exception as e:
+            print(e)
+            time.sleep(15)
